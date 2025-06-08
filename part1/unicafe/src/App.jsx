7df8b52f0823fd2b0b31cb70f2ec3fefbea9f014
yuliya.shibaeva <yuliya.shibaeva.ys@gmail.com>
2025-06-08 16:22:02 +0300

    completed ex. 1.7.-1.8.

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -13,6 +13,32 @@ const Button = ({ onClick, text }) => {
   return <button onClick={onClick}>{text}</button>;
 };
 
+const Statistics = ({ good, neutral, bad }) => {
+  const total = () => {
+    return good + neutral + bad;
+  };
+
+  const avg = () => {
+    return (good*1 + bad*-1)/total();
+  };
+
+  const goodPercent = () => {
+    return good/total()*100;
+  };
+  
+  return (
+    <div>
+      <Header headerText="statistics" />
+      <StatItem text="good" value={good} />
+      <StatItem text="neutral" value={neutral} />
+      <StatItem text="bad" value={bad} />
+      <StatItem text="all" value={total()} />
+      <StatItem text="average" value={avg()} />
+      <StatItem text="positive" value={`${goodPercent()} %`} />
+    </div>
+  );
+};
+
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
@@ -24,10 +50,7 @@ const App = () => {
       <Button text="good" onClick={() => setGood(good + 1)} />
       <Button text="neutral" onClick={() => setNeutral(neutral + 1)}/>
       <Button text="bad" onClick={() => setBad(bad + 1)} />
-      <Header headerText="statistics" />
-      <StatItem text="good" value={good} />
-      <StatItem text="neutral" value={neutral} />
-      <StatItem text="bad" value={bad} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
