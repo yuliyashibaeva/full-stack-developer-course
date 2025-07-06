cb4f5a1d0166f6ae46c18b05feabbfd531506918
yuliya.shibaeva <yuliya.shibaeva.ys@gmail.com>
2025-07-06 22:13:22 +0300

    completed ex. 2.1.-2.2.

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,80 @@
+const Header = ({ coursename }) => {
+  return (
+    <div>
+      <h1>{coursename}</h1>
+    </div>
+  );
+};
+
+const Part = ({ part, exercises }) => {
+  return (
+    <p>
+      {part} {exercises}
+    </p>
+  );
+};
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part) => (
+        <Part key={part.id} part={part.name} exercises={part.exercises} />
+      ))}
+    </div>
+  );
+};
+
+const Total = ({ parts }) => {
+  return (
+    <p>
+      <strong>
+        total of{" "}
+        {parts.reduce((totalNum, part) => totalNum + part.exercises, 0)}{" "}
+        exercises
+      </strong>
+    </p>
+  );
+};
+
+const Course = ({ course }) => {
+  return (
+    <>
+      <Header coursename={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </>
+  );
+};
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+      {
+        name: "Redux",
+        exercises: 11,
+        id: 4,
+      },
+    ],
+  };
+
+  return <Course course={course} />;
+};
+
+export default App;
