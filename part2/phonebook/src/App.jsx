b80ffc54ed4ff9ed6f29131d352909f954bdb0cd
yuliya.shibaeva <yuliya.shibaeva.ys@gmail.com>
2025-07-31 20:51:32 +0300

    completed ex. 2.11.

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,20 +1,26 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Persons from "./components/Persons"
 import PersonForm from './components/PersonForm'
 import Filter from './components/Filter'
 
-const defaultPersons = [
-  { name: 'Arto Hellas', number: '040-123456', id: 1 },
-  { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-  { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-  { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-];
-
 const App = () => {
-  const [persons, setPersons] = useState(defaultPersons) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilterValue, setNewFilterValue] = useState('')
+
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }  
+
+  useEffect(hook, []);
 
   const isPersonAdded = () => persons.some((person) => person.name === newName);
 
